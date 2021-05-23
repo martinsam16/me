4ca713065b6448383e7d566dbff083d73fbb44bd
martinsam16 <martinsam1610@outlook.com>
2021-05-22 23:12:09 -0500

    feat: create vue project and add navbar

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <Navbar/>
+
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery';
+import jQuery from 'jquery';
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  mounted() {
+    $('[data-toggle="collapsible-nav"]').on('click', function(e){
+      console.log(e);
+      var target = ($(this).data('target'));
+      $('#' + target).toggleClass('show');
+    });
+
+    $(document).ready(function(){
+      if(window.innerWidth >= 992) {
+        $('#collapsible-nav').addClass('show');  //Show navigation menu in bigger screens by default.
+      } else {
+        $('#collapsible-nav').removeClass('show');
+      }
+
+      if ($('.hover-box').length) {
+        setHoverBoxPerspective();
+      }
+    });
+
+    $(window).resize(
+        function() {
+          if ($('.hover-box').length) {
+            setHoverBoxPerspective();
+          }
+        }
+    );
+
+    function setHoverBoxPerspective() {
+      $('.hover-box').css({
+        'perspective': function () {
+          return Math.max( $(this).width(), $(this).height() ) * 2 + 50;
+        }
+      });
+    }
+
+
+    var classNames = ['in-up', 'in-right', 'in-down', 'in-left', 'out-up', 'out-right', 'out-down', 'out-left']; // Animation classes.
+
+    $('.hover-box').hover(
+        function (event) {
+          var direction = "up";
+          if(jQuery.fn.entry){ //Check if entry js file is loaded.
+            direction = $(this).entry({ e: event }); // Get mouse in direction.
+          }
+
+          $(this).removeClass(classNames.join(" ")); // Remove existing animation classes.
+          $(this).addClass("in-" + direction); //Add mouse in animation
+
+        },
+
+        function (event) {
+
+          var direction = "up";
+          if(jQuery.fn.entry){
+            direction = $(this).entry({ e: event }); // Get mouse out direction.
+          }
+
+          $(this).removeClass(classNames.join(" "));
+          $(this).addClass("out-" + direction); //Add mouse out animation
+
+        }
+    );
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Mukta:300,400,500,600,700&display=swap');
+  @import 'assets/css/me.css';
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <button class="btn btn-white btn-share ml-auto mr-3 ml-md-0 mr-md-auto">
+      <img src="./../assets/images/share.svg" alt="share" class="btn-img">
+      SHARE
+    </button>
+    <nav class="collapsible-nav" id="collapsible-nav">
+      <a href="#" class="nav-link active">HOME</a>
+      <a href="#" class="nav-link">PORTFOLIO</a>
+      <a href="#" class="nav-link">BLOG</a>
+    </nav>
+    <button class="btn btn-menu-toggle btn-white rounded-circle" data-toggle="collapsible-nav"
+            data-target="collapsible-nav"><img src="./../assets/images/hamburger.svg" alt="hamburger"></button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
