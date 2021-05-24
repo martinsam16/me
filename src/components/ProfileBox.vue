50ceb9274e08ebc4c62e251bda80bd7d5d6a8fed
martinsam16 <martinsam1610@outlook.com>
2021-05-23 20:13:46 -0500

    feat: add router, change logic for views

---
+++ src/components/ProfileBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside>
+    <Profile :name="personal_information.short_name"
+             :degree="personal_information.degree"
+             :linkedin="personal_information.linkedin"
+             :github="personal_information.github"
+             :twitter="personal_information.twitter"/>
+    <PersonalInformation
+        :location="personal_information.location"
+        :birthday="personal_information.birthday"
+        :cv="personal_information.cv"/>
+  </aside>
+</template>
+
+<script>
+import Profile from "@/components/Profile";
+import PersonalInformation from "@/components/PersonalInformation";
+import personal_information from "@/data/me.json";
+
+export default {
+  name: "ProfileBox",
+  components: {
+    Profile,
+    PersonalInformation
+  },
+  data() {
+    return {personal_information};
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Portfolio from '../views/Portfolio.vue'
+import Blog from '../views/Blog.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
