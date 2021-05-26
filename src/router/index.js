75ee4d754fb3e2a5004b682f6abdfc5aa691f2da
martinsam16 <martinsam1610@outlook.com>
2021-05-25 19:26:27 -0500

    feat: 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,40 @@ import Home from '../views/Home.vue'
 import Portfolio from '../views/Portfolio.vue'
 import Blog from '../views/Blog.vue'
 import Learning from "@/views/Learning";
+import NotFound from "@/views/NotFound";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/learning',
-    name: 'Learning',
-    component: Learning
-  },
-  {
-    path: '/portfolio',
-    name: 'Portfolio',
-    component: Portfolio
-  },
-  {
-    path: '/blog',
-    name: 'Blog',
-    component: Blog
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/learning',
+        name: 'Learning',
+        component: Learning
+    },
+    {
+        path: '/portfolio',
+        name: 'Portfolio',
+        component: Portfolio
+    },
+    {
+        path: '/blog',
+        name: 'Blog',
+        component: Blog
+    }, {
+        path: '/:pathMatch(.*)*',
+        component: NotFound
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
