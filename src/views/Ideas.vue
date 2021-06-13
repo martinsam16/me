c60bc6e1d120b5dbc7c440db929407d73b5d7f7d
martinsam16 <martinsam1610@outlook.com>
2021-06-13 05:30:27 -0500

    feat: ideas page mockup

--- src/views/Ideas.vue
+++ src/views/Ideas.vue
@@ -2,7 +2,36 @@
   <div class="content-wrapper">
     <ProfileBox/>
     <main>
-      Ideas page 👷‍♂️
+      <div class="widget card">
+        <div class="card-body">
+          <div class="widget-content">
+            <h5 class="widget-title card-title">Idea</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis dicta, molestias
+              perspiciatis quis sit tempore vel. A accusantium aliquam aperiam, consectetur deserunt eaque expedita
+              facere, incidunt provident, quaerat sed!</p>
+          </div>
+        </div>
+      </div>
+      <div class="widget card">
+        <div class="card-body">
+          <div class="widget-content">
+            <h5 class="widget-title card-title">Idea</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis dicta, molestias
+              perspiciatis quis sit tempore vel. A accusantium aliquam aperiam, consectetur deserunt eaque expedita
+              facere, incidunt provident, quaerat sed!</p>
+          </div>
+        </div>
+      </div>
+      <div class="widget card">
+        <div class="card-body">
+          <div class="widget-content">
+            <h5 class="widget-title card-title">Idea</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis dicta, molestias
+              perspiciatis quis sit tempore vel. A accusantium aliquam aperiam, consectetur deserunt eaque expedita
+              facere, incidunt provident, quaerat sed!</p>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
